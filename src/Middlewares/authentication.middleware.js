de2310c8da45163ab62dff5d76e74e57f6e43c3d
Ahmed Dorgham <ahmeddorgham779@gmail.com>
2025-01-12 01:59:19 +0200

    Add authentication and authorization

---
+++ src/Middlewares/authentication.middleware.js
@@ -0,0 +1,38 @@
+import { User } from "../../DB/Models/index.js";
+import { ErrorClass } from "../Utils/error-class.utils";
+import jwt from "jsonwebtoken";
+
+export const authentication = async (req, res, next) => {
+  try {
+    const { token } = req.headers;
+
+    if (!token || !token.startsWith(`${process.env.BEARER} `)) {
+      return next(new ErrorClass("Invalid token format", 400));
+    }
+
+    const actualToken = token.split(" ")[1];
+    let data;
+    try {
+      data = jwt.verify(actualToken, process.env.LOGIN_SECRET_KEY);
+    } catch (err) {
+      if (err.name === "TokenExpiredError") {
+        return next(new ErrorClass("Token expired", 401));
+      }
+      return next(new ErrorClass("Invalid token", 401));
+    }
+
+    if (!data?.userId) {
+      return next(new ErrorClass("Invalid token payload", 400));
+    }
+
+    const user = await User.findById(data.userId).select("id name email");
+    if (!user) {
+      return next(new ErrorClass("User not found", 404));
+    }
+
+    req.authUser = user;
+    next();
+  } catch (error) {
+    return next(new ErrorClass("Authentication failed", 500));
+  }
+};
---
+++ src/Middlewares/authorization.middleware.js
@@ -0,0 +1,20 @@
+import { ErrorClass } from "../Utils/error-class.utils";
+
+export const authorization = (allowedRoles) => {
+  if (!Array.isArray(allowedRoles) || allowedRoles.length === 0) {
+    return next(new ErrorClass("Invalid or missing allowedRoles Array", 400));
+  }
+
+  return (req, res, next) => {
+    const { role } = req.authUser;
+
+    if (!role) {
+      return next(new ErrorClass("User role not found", 403));
+    }
+
+    if (!allowedRoles.includes(role)) {
+      return next(new ErrorClass("Unauthorized : You do not have access to this resource", 401));
+    }
+    next();
+  };
+};
